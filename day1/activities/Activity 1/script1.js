64d18c33adef576e30c58e7da9abf6b408094518
Chloe Moss <chloe@chloeintech.com>
2023-06-09 17:21:16 +0100

    first commit

---
+++ day1/activities/Activity 1/script1.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  const x = document.getElementsByClassName("eatButton");
+  if (x === 'i'){
+    document.getElementById('cookieImage').style.visibility = "visable";
+
+  } 
+  else{
+    document.getElementById('cookieImage').style.visibility = "hidden";
+
+  }
+}
---
+++ day1/notes/script.js
@@ -0,0 +1,13 @@
+// // Selecting Elements
+// // Select multiple elements
+// document.getElementsByTagName("li") 
+// document.getElementsByClassName("shoppingItem")
+// document.querySelectorAll()
+
+// //
+// document.getElementById()// choose specific ID
+// document.querySelector()// would find the first element selected
+
+const heading= document.getElementById("heading")
+
+H
